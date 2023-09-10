2892a6f17171f692fd6026de472b2ea3b4e34700
Volodymyr Voloshyn <riokin2011@gmail.com>
2023-09-10 16:39:53 +0200

    Create CreateStoreModal

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,22 @@
-import { UserButton } from '@clerk/nextjs'
+'use client'
+
+import { useEffect } from 'react'
+
+import { useStoreModal } from '@/hooks/use-store-modal'
 
 export default function Home() {
+  const { isOpen, onClose, onOpen } = useStoreModal()
+
+  useEffect(() => {
+    if (!isOpen) {
+      onOpen()
+    }
+  }, [isOpen, onClose, onOpen])
+
   return (
     <div className=''>
+      hello
       <p>Hello Admin Dashboard</p>
-      <UserButton afterSignOutUrl='/' />
     </div>
   )
 }
---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Modal } from "@/components/ui/modal";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+const formSchema = z.object({
+  name: z.string().min(3),
+});
+
+export const StoreModal = () => {
+  const { isOpen, onClose } = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+  const { handleSubmit, control } = form;
+
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <Modal
+      title="Create store"
+      description="Add a new store to manage products and categories"
+      isOpen={isOpen}
+      onClose={onClose}
+    >
+      <div className="space-y-4 py-2 pb-4">
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormField
+              control={control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="E-Commerce" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex w-full items-center justify-end space-x-2 pt-6">
+              <Button variant="outline" onClick={onClose}>
+                Cancel
+              </Button>
+              <Button type="submit">Continue</Button>
+            </div>
+          </form>
+        </Form>
+      </div>
+    </Modal>
+  );
+};
