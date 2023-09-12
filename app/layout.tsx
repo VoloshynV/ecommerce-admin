be1b52c99ffd5250905aa08b81eb7b03ebe28370
Volodymyr Voloshyn <riokin2011@gmail.com>
2023-09-12 11:30:11 +0200

    Add theme switcher

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
 import { ModalProvider } from "@/providers/modal-provider";
+import { ThemeProvider } from "@/providers/theme-provider";
 import { ToastProvider } from "@/providers/toast-provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -23,9 +24,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ModalProvider />
-          <ToastProvider />
-          {children}
+          <ThemeProvider attribute="class" defaultTheme="light">
+            <ModalProvider />
+            <ToastProvider />
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,6 +3,7 @@ import { redirect } from "next/navigation";
 
 import { MainNav } from "@/components/main-nav";
 import { StoreSwitcher } from "@/components/store-switcher";
+import { ThemeSwitcher } from "@/components/ui/theme-switcher";
 import prismadb from "@/lib/prismadb";
 
 const Navbar = async () => {
@@ -20,6 +21,7 @@ const Navbar = async () => {
         <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
+          <ThemeSwitcher />
           <UserButton afterSignOutUrl="/" />
         </div>
         <div />
